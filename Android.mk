11726a238ca4f3850c1cf9418dbddf86b3656b6e
Will Shackleton <w.shackleton@gmail.com>
2015-01-05 13:58:55 +0000

    Modified build system to work on android-ndk-r10d

--- Android.mk
+++ Android.mk
@@ -34,10 +34,8 @@ include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS:=
 LOCAL_MODULE:=libext
 
-# LOCAL_MODULE_CLASS must be defined before calling $(local-intermediates-dir)
-#
 LOCAL_MODULE_CLASS := STATIC_LIBRARIES
-intermediates := $(call local-intermediates-dir)
+intermediates := $(shell realpath obj)
 
 LOCAL_C_INCLUDES:= \
 	$(LOCAL_PATH)/include/ \
@@ -49,7 +47,7 @@ LOCAL_CFLAGS+=-D_INIT=$*_init
 LOCAL_CFLAGS+=-DIPTABLES_VERSION=\"1.3.7\"
 
 PF_EXT_SLIB:=ah addrtype comment 2connmark conntrack 2dscp 2ecn esp 
-PF_EXT_SLIB+=hashlimit helper icmp iprange length limit mac multiport #2mark
+PF_EXT_SLIB+=hashlimit helper icmp iprange length limit mac # multiport #2mark
 PF_EXT_SLIB+=owner physdev pkttype policy realm sctp standard state tcp 
 PF_EXT_SLIB+=2tcpmss 2tos 2ttl udp unclean CLASSIFY CONNMARK DNAT LOG #DSCP ECN
 PF_EXT_SLIB+=MASQUERADE MIRROR NETMAP NFQUEUE NOTRACK REDIRECT REJECT #MARK
@@ -61,18 +59,19 @@ EXT_FUNC+=$(foreach T,$(PF_EXT_SLIB),ipt_$(T))
 
 GEN_INITEXT:= $(intermediates)/extensions/gen_initext.c
 $(GEN_INITEXT): PRIVATE_PATH := $(LOCAL_PATH)
-$(GEN_INITEXT): PRIVATE_CUSTOM_TOOL = $(PRIVATE_PATH)/extensions/create_initext "$(EXT_FUNC)" > $@
-$(GEN_INITEXT): PRIVATE_MODULE := $(LOCAL_MODULE)
 $(GEN_INITEXT):
-	$(transform-generated-source)
-
-$(intermediates)/extensions/initext.o : $(GEN_INITEXT)
-
-LOCAL_GENERATED_SOURCES:= $(GEN_INITEXT)
+	mkdir -p $(intermediates)/extensions/
+	echo
+	echo EXT_FUNC
+	echo $(EXT_FUNC)
+	echo
+	echo PF_EXT_SLIB
+	echo $(PF_EXT_SLIB)
+	$(PRIVATE_PATH)/extensions/create_initext "$(EXT_FUNC)" > $@
 
 LOCAL_SRC_FILES:= \
 	$(foreach T,$(PF_EXT_SLIB),extensions/libipt_$(T).c) \
-	extensions/initext.c
+	$(GEN_INITEXT)
 
 LOCAL_STATIC_LIBRARIES := \
 	libc
